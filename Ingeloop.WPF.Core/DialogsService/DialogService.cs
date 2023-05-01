836e15e6683f6a92c0463a4b43e204f62f9aebed
Léo SALVADOR <89361568+Ingeloop@users.noreply.github.com>
2023-05-01 20:29:22 +0200

    Add resources copy for Dialogs

--- Ingeloop.WPF.Core/DialogsService/DialogService.cs
+++ Ingeloop.WPF.Core/DialogsService/DialogService.cs
@@ -12,6 +12,7 @@ namespace Ingeloop.WPF.Core
 {
     public static class DialogService
     {
+        private readonly static List<string> resourcesToCopy = new List<string> { "Primary", "PrimaryBrush", "Secondary", "SecondaryBrush" };
         public static Window DialogHost { get; private set; }
 
         public static readonly DependencyProperty DialogHostProperty = DependencyProperty.RegisterAttached("DialogHost",
@@ -77,6 +78,15 @@ namespace Ingeloop.WPF.Core
             if (dialogWindow == null)
             {
                 return null;
+            }
+
+            foreach(string resourceToCopy in resourcesToCopy)
+            {
+                object resourceValue = DialogHost.TryFindResource(resourceToCopy);
+                if (resourceValue != null)
+                {
+                    dialogWindow.Resources[resourceToCopy] = resourceValue;
+                }
             }
 
             dialogWindow.DataContext = dialogViewModel;
